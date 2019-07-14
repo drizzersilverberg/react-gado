e341e33421251bec300d832d71614082954a1d3c
Dendi Handian <dhn@voxteneo.asia>
2019-07-14 21:25:14 +0700

    simple get videos from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { hot } from 'react-hot-loader/root'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <h1>Videos</h1>
-        </header>
-      </div>
-    );
-  }
+function App () {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
+        response => response.json()
+      );
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Videos</h1>
+      </header>
+      {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default hot(App);
